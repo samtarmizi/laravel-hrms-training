fa754581943b7df6f8d893f4d8f3c1c6efb4d962
TARMIZI SANUSI <realtarmizisanusi@gmail.com>
2024-09-24 15:21:37 +0800

    oauth setup

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/passports', [App\Http\Controllers\PassportController::class, 'index'])->name('passport:index');
+Route::get('/passports/create', [App\Http\Controllers\PassportController::class, 'create'])->name('passport:create');
+Route::post('/passports/create', [App\Http\Controllers\PassportController::class, 'store'])->name('passport:store');
